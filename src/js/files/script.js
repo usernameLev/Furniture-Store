fb2fe1273452e4ea5f1e57708be7f7afb3eded41
Lev <tyanlev1989@gmail.com>
2023-01-16 17:52:06 +0500

    Completed Header slider

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -38,4 +38,18 @@ window.onload = function () {
       }
     }
   }
+
+  // Header
+  const headerElement = document.querySelector('.header');
+
+  const callback = function (entries, observer) {
+    if (entries[0].isIntersecting) {
+      headerElement.classList.remove('_scroll');
+    } else {
+      headerElement.classList.add('_scroll');
+    }
+  };
+
+  const headerObserver = new IntersectionObserver(callback);
+  headerObserver.observe(headerElement);
 };
